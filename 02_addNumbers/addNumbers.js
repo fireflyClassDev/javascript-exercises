7e62e7457bb95fc5d26d5159e7ebe8f6d71a3ee5
fireflyClassDev <242595895+fireflyClassDev@users.noreply.github.com>
2025-11-21 17:20:18 +0200

    finish the Data Types and Conditionals Foundations Course

--- 02_addNumbers/addNumbers.js
+++ 02_addNumbers/addNumbers.js
@@ -4,10 +4,10 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
 }
-
+ 
 // Do not change this
 module.exports = addNumbers;
